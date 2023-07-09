059f98261fa862029f0c8f84c0e5f0b687679b51
Ruslan-Gubin <109853388+Ruslan-Gubin@users.noreply.github.com>
2023-07-09 08:25:23 +0300

    deploy 3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 
 const nextConfig = {
   reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, './src');
+    return config;
+  },
 }
 
 module.exports = nextConfig
